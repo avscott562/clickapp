4138b17792d46219f6f7cafca5a51fdf498f501c
Scottie <avscott@pacbell.net>
2019-12-14 03:19:11 -0800

    add checked function

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import "../styles/card.css";
 
+function clicked() {
+    if(!this.isClicked) {
+        this.isClicked = true;
+    } else {
+        console.log('game over!');
+    }
+}
+
 function Card(props) {
     return (
         <div className="col-3">
             <div className="tile mb-3">
-                <img className="tile-image" src={props.src} data-id={props.id} alt={props.alt} isClicked={props.isClicked}></img>
+                <img 
+                    className="tile-image" 
+                    src={props.src} 
+                    data-id={props.id} 
+                    alt={props.alt} 
+                    isClicked={props.isClicked}></img>
             </div>
         </div>
     );
